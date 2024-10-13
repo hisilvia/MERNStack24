f2527be37ff3fe5d6699cd3d296232e6f5f2eda7
hisilvia <silviamatthews2018@gmail.com>
2024-10-13 12:56:42 -0500

    variables

---
+++ ES6/Variables.js
@@ -0,0 +1,11 @@
+//ECMAScript 2015 is also known as ES6 and ECMAScript 6.
+//1. DataFile.js -10082024
+// JS - function, var
+// 2015 => JS => ES6 ==> ES.Next
+// ES - EcmaScript
+
+//2. Variables.js  -10082024
+//Unlike var, Let allows the script to restrict access to the variable to the nearest enclosing block
+//and ensures we dont have re-declaration of a variable.
+
+//Const declaration creats a read-only reference to a value.
