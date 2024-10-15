5e1a9cd8a686815610e409b79953cff8b3f869a2
hisilvia <silviamatthews2018@gmail.com>
2024-10-15 08:39:15 -0500

    class

--- ES6/MapAndSet.js
+++ ES6/MapAndSet.js
@@ -1,1 +1,88 @@
 Date: 10/14/2024
+//1.Map : is the data structure created to store data in the form of key value pair
+//  Strong Map - when our keys are of string type or alphanumeric type
+//  Weak Map - this allows us to create keys such as numbers, functions, objects etc
+//  There are several methods, properties attached to map to validate and access the values present against key
+
+//[{key : "value"}]
+let myMap = new Map();
+
+myMap.set("UserName", "Anthony")
+console.log(myMap.size)
+//The entries() method returns an iterator object with the [key,values] in a map.
+console.log(myMap.entries()) //==>[Map Entries] {['UserName','Anthony']}
+
+
+
+//creating a weak map
+let keyString = 'a string', keyObj = {}, keyFunc = function() {}, keyNum = 2000;
+
+myMap.set(keyString, "String is key for this!!")
+myMap.set(keyObj, "Object is key for this!!")
+myMap.set(keyFunc, "Function is key for this!!")  
+myMap.set(keyNum, "Number is key for this!!")
+
+
+console.log(myMap.size)         //5
+console.log(myMap.entries())    //4#==>[[Function: keyFunc],'Function is key for this!!']
+
+//The get(key) method gets the value of a key in a map
+console.log(myMap.get(2000))    //==>Number is key for this!!
+console.log(myMap.get({}))      //==>undefined : object is refered by reference variable not a value type, for each object a separate memory allocated on heap
+console.log(myMap.get(keyObj))  //==>Object is key for this!!  (cause it can only access via same reference variable)
+
+
+// let newObject = {};
+// console.log(myMap.get(newObject))
+// console.log(myMap.get(keyObj))
+
+myMap.delete(keyNum)
+console.log(myMap.entries())
+
+console.log(myMap.has(keyString))      //==>true
+console.log(myMap.has("random key"))   //==>false
+console.log(myMap.keys())              //list of all keys
+
+myMap.clear(); //removes all data
+console.log(myMap.entries()) 
+console.log("------------------------------")
+
+//2. Set : is the data structure which helps us create unique data without many comparisons
+//   Now wanna display one name at a time
+// let TraineeList = []
+// TraineeList.push("Duncan")
+//before adding - Same user name we need to check for each time if its already present or not
+
+let TraineeList = new Set(["Silvia", "Afroj", "Yao", "Mark", "Nilay"])
+
+TraineeList.add("Yao")                //==>no error, but it isnt added into the list
+
+console.log(TraineeList.entries())
+
+TraineeList.add("Sierra")
+console.log(TraineeList.entries())
+console.log(TraineeList.size)
+
+TraineeList.add("Ben")
+console.log(TraineeList.values())
+console.log(TraineeList.size)
+
+//Create two examples of your own choice to make a map and a weak map
+// and a list of unique names of 10 states of your favrourite country you wish to visit on world tour
+console.log("---------------Practice---------------")
+let myWishList = new Map();
+myWishList.set("first","Xi'an");
+myWishList.set("second","Shijiazhuang");
+myWishList.set("third","Guilin");
+myWishList.set("forth","Xiamen");
+myWishList.set("fifth","Qindao");
+
+//a weak map
+let strKey = "Egypt",  objKey = {}, funcKey = function() {}, numKey = 9;
+myWishList.set(strKey, "pyramid");
+myWishList.set(objKey, "Lhasa");
+myWishList.set(funcKey, "HongKong");
+myWishList.set(numKey, "Paris");
+myWishList.set("Forest", "Norway");
+
+console.log(myWishList.entries());
--- ES6/class.js
+++ ES6/class.js
@@ -1,5 +1,9 @@
 Date: 10/14/2024
-//Class -
+//Class - has been added in ES6 as a special and upgraded function to give us feature similar to
+// other class based programming languages
+
+//class keyword is used to create class and we can create associated methods 
+//  as we did in cunstructor function <without using this>
 
 class Area {
 
@@ -27,3 +31,38 @@ console.log(areaObj.Rectangle())
 
 //Task - create a class named as account accepting 3 or more params like name, acct type etc and
 //       has three methods to show balance, user details, and account offers
+console.log("---------------Practice---------------")
+class Account {
+    constructor(id, name, type){
+        this.id = id;
+        this.name = name;
+        this.type = type;
+
+    }
+
+    Balance = function(debit=0, credit=0) {
+        return debit-credit;
+    }
+
+    GetUserDetails = ()=>{
+        console.log(`
+                        ${this.id}
+                        ${this.name}
+                        ${this.type}
+                     `)
+    }
+
+    AcctOffers = function(){
+        if (this.type === "debit")
+            return console.log("Saving account");
+
+        else
+            return console.log("Credit card");
+    }
+}
+
+let acctObj = new Account(7968, "Lilly", "debit");
+console.log(acctObj.Balance(50000,39500));
+console.log(acctObj.GetUserDetails());
+console.log("*****")
+console.log(acctObj.AcctOffers());
