901aa2effab658a903484e4b5f91bbdfc463869f
hisilvia <104528370+hisilvia@users.noreply.github.com>
2025-01-30 12:04:31 -0600

    Update ProductDataModel.js

--- assReactapi/DataModel/ProductDataModel.js
+++ assReactapi/DataModel/ProductDataModel.js
@@ -1,7 +1,7 @@
 let mongooseObj = require("mongoose");
 schemaObj = mongooseObj.Schema;
 
-mongooseObj.connect("mongodb+srv://smatthews:12345@cluster0.32ftxoe.mongodb.net/mernstack24");
+mongooseObj.connect("mongodb+srv://smatthews:?.mongodb.net/mernstack24");
 
 let productSchema = new schemaObj({
     productName: {type: String, required: true},
